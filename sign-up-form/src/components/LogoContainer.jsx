720eedf1d6b81e857dd298b370b43d89bb252e94
Kirsten <kmalcaba@gmail.com>
2022-06-05 17:15:12 -0700

    Upload media assets'

--- sign-up-form/src/components/LogoContainer.jsx
+++ sign-up-form/src/components/LogoContainer.jsx
@@ -1,7 +1,14 @@
+import bg from "../media/bg-image.jpg";
+import logo from "../media/odin-lined.png";
+
 const LogoContainer = () => {
   return (
     <div className='relative flex flex-col'>
-      <div className=''></div>
+      <img src={bg} alt='Black keyboard' className='max-h-screen' />
+      <div className='absolute p-8 w-full flex justify-center items-center bg-black opacity-75 top-20'>
+        <img src={logo} alt='Odin Project Logo' className='w-12 inline-block' />
+        <h1 className='text-4xl inline-block mx-5 text-white'>Welcome</h1>
+      </div>
     </div>
   );
 };
